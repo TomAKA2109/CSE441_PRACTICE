bda4b1dc072bc3a9301a34220a09c00b95cabb90
Tom <tomdattebayo@gmail.com>
2024-09-21 14:46:08 +0700

    caculator

---
+++ app/src/main/java/com/example/caculator/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.caculator;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edtA, edtB, edtKQ;
+    Button btnTong, btnHieu, btnTich, btnThuong;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edtA = findViewById(R.id.edtA);
+        edtB = findViewById(R.id.edtB);
+        edtKQ = findViewById(R.id.edtKQ);
+        btnTong = findViewById(R.id.btnTong);
+        btnHieu = findViewById(R.id.btnHieu);
+        btnTich = findViewById(R.id.btnTich);
+        btnThuong = findViewById(R.id.btnThuong);
+
+    }
+}
